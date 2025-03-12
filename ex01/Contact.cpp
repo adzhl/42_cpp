dacfc7f054b2a5615b2d75bda7ea6bd3a376a993
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-03-12 13:19:29 +0800

    WIP for ex01

---
+++ ex01/Contact.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 12:09:53 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/03/12 12:46:49 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+void Contact::setContact()
+{
+    std::cout << "Enter First Name: ";
+    std::getline(std::cin, first_name);
+    std::cout << "Enter Last Name: ";
+    std::getline(std::cin, last_name);
+    std::cout << "Enter Nickname: ";
+    std::getline(std::cin, nickname);
+    std::cout << "Enter Phone Number: ";
+    std::getline(std::cin, phone_number);
+    std::cout << "Enter Darkest Secret: ";
+    std::getline(std::cin, darkest_secret);
+}
+
+void Contact::getContact()
+{
+    std::cout << "First Name: " << first_name << '\n';
+    std::cout << "Last Name: " << last_name << '\n';
+    std::cout << "Nickname: " << nickname << '\n';
+    std::cout << "Phone Number: " << phone_number << '\n';
+    std::cout << "Darkest Secret: " << darkest_secret << '\n';
+}
+
+int main()
+{
+    Contact myContact;
+    std::string input;
+
+    while (true)
+    {
+        std::cout << "Enter Command (ADD, SEARCH, EXIT): ";
+        std::getline(std::cin, input);
+
+        if (input == "ADD")
+            myContact.setContact();
+        else if (input == "SEARCH")
+            myContact.getContact();
+        else if (input == "EXIT")
+            break;
+        
+        system("clear");
+    }
+    return (0);
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 10:03:16 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/03/12 12:46:18 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <string>
+# include <cstdlib>
+
+class Contact {
+    private:
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+        std::string phone_number;
+        std::string darkest_secret;
+
+    public:
+        void setContact();
+        void getContact();
+};
+
+#endif
