40c7ad3f6fa2a22598a351d57399d8821d60cb5a
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-03-25 12:20:45 +0800

    Add ex04

---
+++ module_01/ex04/main.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 09:19:16 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/03/25 10:49:48 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void replaceLine(std::string &line, const std::string &s1, const std::string &s2) {
+    size_t pos = 0;
+    while ((pos = line.find(s1, pos)) != std::string::npos) {
+        line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+        pos += s2.length();
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cerr << "Usage: ./sed <filename> <string to replace> <new string>\n"; return (1); }
+
+    std::string s1 = argv[2];
+    if (s1.empty()) {
+        std::cerr << "Error: string to replace cannot be empty\n"; return (1); }
+
+    std::ifstream input(argv[1]);
+    if (!input) {
+        std::cerr << "Error: Cannot open file\n"; return (1); }
+
+    std::string outputFilename = std::string(argv[1]) + ".replace";
+    std::ofstream output(outputFilename.c_str());
+    if (!output) {
+        std::cerr << "Error: Cannot create output file\n"; return (1); }
+
+    std::string line;
+    while (std::getline(input, line)) {
+        replaceLine(line, argv[2], argv[3]);
+        output << line;
+        if (!input.eof())
+            output << '\n';
+    }
+
+    return (0);
+}
