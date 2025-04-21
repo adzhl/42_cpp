49334cbf92981583929faa11cabf62841551344f
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-04-21 17:22:01 +0800

    Working main without leaks

--- module_04/ex03/main.cpp
+++ module_04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 15:47:26 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/04/21 13:10:51 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/04/21 17:21:25 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,89 +15,81 @@
 #include "Cure.hpp"
 #include "MateriaSource.hpp"
 
-int main()
-{
+void    basicTest() {
     std::cout << BLUE << "\n=== BASIC TEST ===\n\n" << RESET;
+
     IMateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
-    
+
     ICharacter* me = new Character("me");
-    
     AMateria* tmp;
+
     tmp = src->createMateria("ice");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
-    
+
     ICharacter* bob = new Character("bob");
-    
     me->use(0, *bob);
     me->use(1, *bob);
-    
-    std::cout << BLUE << "\n---- Destroying objects ----\n" << RESET;
+
+    std::cout << BLUE << "\n--- Cleanup ---\n" << RESET;
     delete bob;
     delete me;
     delete src;
+
+}
+
+int main() {
+    basicTest();
     
     std::cout << BLUE << "\n=== ADDITIONAL TEST ===\n\n" << RESET;
-    
-    // Test creating unknown materia
-    src = new MateriaSource();
+    IMateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
-    
-    tmp = src->createMateria("unknown");
-    if (tmp == NULL)
-        std::cout << "Creating unknown materia returned NULL as expected" << std::endl;
-    
-    // Test full inventory
-    me = new Character("me");
-    tmp = src->createMateria("ice");
-    me->equip(tmp);  // Equip slot 0
-    tmp = src->createMateria("ice");
-    me->equip(tmp);  // Equip slot 1
-    tmp = src->createMateria("ice");
-    me->equip(tmp);  // Equip slot 2
-    tmp = src->createMateria("ice");
-    me->equip(tmp);  // Equip slot 3
-    tmp = src->createMateria("ice");
-    me->equip(tmp);  // Should not equip (inventory full)
-    delete tmp;
+    src->learnMateria(new Cure());
 
-    std::cout << BLUE << "\n---Equipped 5 materias (should only store 4) ---\n" << RESET;
-    
-    // Test using materia from invalid slot
-    std::cout << "Using materia from valid slot (0): ";
-    me->use(0, *me);
-    std::cout << "Using materia from invalid slot (5): ";
-    me->use(5, *me);  // Should do nothing
-    std::cout << '\n';
-    std::cout << "Using materia from invalid slot (-1): ";
-    me->use(-1, *me);  // Should do nothing
-    std::cout << '\n';
-    
-    // Test unequipping
-    std::cout << "Unequipping slot 1" << std::endl;
-    me->unequip(1);
-    std::cout << "Using unequipped slot 1: ";
-    me->use(1, *me);  // Should do nothing
-    
-    // Test character copy
-    Character* originalChar = new Character("original");
-    tmp = src->createMateria("ice");
-    originalChar->equip(tmp);
-    
-    Character* copiedChar = new Character(*originalChar);
-    std::cout << "Original character using materia: ";
-    originalChar->use(0, *me);
-    std::cout << "Copied character using materia: ";
-    copiedChar->use(0, *me);
-    
-    std::cout << BLUE << "\n---- Destroying objects ----\n" << RESET;
-    delete copiedChar;
-    delete originalChar;
+    std::cout << BLUE << "\n=== Create Character ===\n" << RESET;
+    ICharacter* me = new Character("me");
+
+    // Store allocated materia so we can delete it later
+    AMateria* ice1 = src->createMateria("ice");
+    AMateria* cure1 = src->createMateria("cure");
+    AMateria* ice2 = src->createMateria("ice");
+    AMateria* cure2 = src->createMateria("cure");
+    AMateria* extra = src->createMateria("ice");  // 5th materia
+
+    // Equip only 4
+    me->equip(ice1);
+    me->equip(cure1);
+    me->equip(ice2);
+    me->equip(cure2);
+
+    // Try equipping 5th (won't be stored), so we delete it manually
+    me->equip(extra);
+    delete extra;
+
+    std::cout << BLUE << "\n=== Using Equipped Materia ===\n" << RESET;
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+    me->use(2, *bob);
+    me->use(3, *bob);
+
+    std::cout << BLUE << "\n=== Unequipping and Manual Delete ===\n" << RESET;
+    me->unequip(2);  // remove ice2 manually
+    delete ice2;
+
+    std::cout << BLUE << "\n=== Deep Copy Test ===\n" << RESET;
+    Character* copy = new Character(*dynamic_cast<Character*>(me));
+    copy->use(0, *bob);
+    copy->use(1, *bob);
+
+    std::cout << BLUE << "\n=== Cleanup ===\n" << RESET;
+    delete copy;
+    delete bob;
     delete me;
     delete src;
-    
-    return 0;
+
+    return (0);
 }
