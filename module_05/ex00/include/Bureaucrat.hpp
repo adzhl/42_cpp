c82a349b655b8d1935910fb0081a7c0e6e123cc5
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-04-24 11:42:44 +0800

    Add ex00

---
+++ module_05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 15:55:08 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/04/24 10:53:24 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# define RESET   "\033[0m"
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define BLUE    "\033[34m"
+
+# include <iostream>
+# include <string>
+
+class Bureaucrat {
+    private:
+        const std::string   _name;
+        int                 _grade;
+
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat&    operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+
+        const std::string&  getName() const;
+        int                 getGrade() const;
+
+        void                incrementGrade();
+        void                decrementGrade();
+
+        // nested class inside Bureaucrat
+        // belongs to Bureaucrat - Bureaucrat::GradeTooHighException
+        class GradeTooHighException : public std::exception {
+            public:
+                const char* what() const throw();
+        } ;
+
+        class GradeTooLowException : public std::exception {
+            public:
+                const char* what() const throw();
+        } ;
+} ;
+
+// Insertion operator overloader
+// How an object of a class should be displayed to the output stream
+// friend : can directly access private members of the class
+std::ostream&               operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
--- module_05/ex00/main.cpp
+++ module_05/ex00/main.cpp
@@ -6,7 +6,76 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:54:22 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/04/23 15:54:24 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/04/24 11:42:01 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+int main() {
+    try {
+        std::cout << YELLOW << "\n== OCF TEST===\n" << RESET;
+
+        Bureaucrat mc("mc", 1);
+        Bureaucrat tara(mc);
+        mc = tara;
+    }
+    catch (std::exception &e) {
+        std::cout << RED << e.what() << '\n' << RESET;
+    }
+
+    try {
+        std::cout << YELLOW << "\n== NORMAL TEST===\n" << RESET;
+
+        Bureaucrat caleb("Caleb", 1);
+        std::cout << GREEN << caleb << '\n' << RESET;
+
+        Bureaucrat sylus("Sylus", 150);
+        std::cout << GREEN << sylus << '\n' << RESET;
+    }
+    catch (std::exception &e) {
+        std::cout << RED << e.what() << '\n' << RESET;
+    }
+
+    try {
+        std::cout << YELLOW << "\n=== GRADE TOO LOW TEST ===\n" << RESET;
+        Bureaucrat xavier("Xavier", 151);
+        std::cout << GREEN << xavier << '\n' << RESET; // This line will be skipped when there is an exception
+    } catch (std::exception &e) {
+        std::cout << RED << e.what() << '\n' << RESET;
+    }
+
+    try {
+        std::cout << YELLOW << "\n=== GRADE TOO HIGH TEST ===\n" << RESET;
+        Bureaucrat rafayel("Rafayel", 0);
+        std::cout << GREEN << rafayel << '\n' << RESET;
+    } catch (std::exception &e) {
+        std::cout << RED << e.what() << '\n' << RESET;
+    }
+
+    try {
+        std::cout << YELLOW << "\n=== INCREMENT TEST ===\n" << RESET;
+        Bureaucrat zayne("Zayne", 2);
+        std::cout << GREEN << zayne << '\n' << RESET;
+        zayne.incrementGrade(); // 2 -> 1
+        std::cout << GREEN << zayne << '\n' << RESET;
+        zayne.incrementGrade(); // 1 -> 0
+        std::cout << GREEN << zayne << '\n' << RESET;
+    } catch (std::exception &e) {
+        std::cout << RED << e.what() << '\n' << RESET;
+    }
+
+    try {
+        std::cout << YELLOW << "\n=== DECREMENT TEST ===\n" << RESET;
+        Bureaucrat mephisto("mephisto", 149);
+        std::cout << GREEN << mephisto << '\n' << RESET;
+        mephisto.decrementGrade(); // 149 -> 150
+        std::cout << GREEN << mephisto << '\n' << RESET;
+        mephisto.decrementGrade(); // 150 -> 151
+        std::cout << GREEN << mephisto << '\n' << RESET;
+    } catch (std::exception &e) {
+        std::cout << RED << e.what() << std::endl << RESET;
+    }
+
+    return 0;
+}
