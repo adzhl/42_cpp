1b9bd2b02b2bdcd8bf11dc9144093a9d33486b32
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-05-06 15:05:03 +0800

    WIP ex00

---
+++ module_06/ex00/include/ScalarConverter.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 08:14:35 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/05/06 15:04:21 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <string>
+
+class ScalarConverter {
+    public:
+        static void convert(std::string literal);
+
+    private:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter& other);
+        ScalarConverter&    operator=(const ScalarConverter& other);
+        ~ScalarConverter();
+} ;
+
+#endif
+
+// Setting constructors as private prevents instantiation
+
+// 1. Receive a string literal
+// 2. Detect its type (char, int, float, double)
+// 3. Convert to all other scalar types
+// 4. Print the result in each type
+
+// Types of casts
+// 1. STATIC CAST
+//  - standard type conversions
+//  - converting between numeric types (int, float, double)
+//  - upcasting in inheritance (Derived* to Base*)
+//
+// 2. DYNAMIC CAST
+//  - safe downcasting in inheritance (Base* to Derived*)
+//  - works with pointers/references to polymorphic classes
+//  - failed downcasting -  pointer:   returns (nullptr)
+//                       -  reference: throws std::bad_cast
+//
+// 3. CONST CAST
+//  - add or remove const/volatile qualifiers from a variable (const int* to int*)
+//  - if the original object was const, removing const and modifying the object is undefined behaviour
+//
+// 4. REINTERPRET_CAST
+//  - bit-level (low level) reinterpretation
+//  - allow casting between unrelated pointer types
+//  - can lead to undefined behaviour
---
+++ module_06/ex00/src/ScalarConverter.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 09:21:36 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/05/06 09:43:36 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+// Constructor
+ScalarConverter::ScalarConverter() {}
+
+// Copy constructor
+ScalarConverter::ScalarConverter(const ScalarConverter& other) { (void)other; }
+
+// Copy assignment operator
+ScalarConverter&    ScalarConverter::operator=(const ScalarConverter& other) { (void)other; return (*this); }
+
+// Destructor
+ScalarConverter::~ScalarConverter() {}
+
+
+
