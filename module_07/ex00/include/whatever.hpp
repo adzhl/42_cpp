68c4e85983ca6bec3a60b5dafc2efe66e035910b
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-05-08 17:24:18 +0800

    WIP ex00

---
+++ module_07/ex00/include/whatever.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:58:45 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/05/08 15:24:16 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+// what is a template?
+// - allows you to write generic & usable code.
+// - works with any type (no need to create separate functions for each type)
+// - avoids code duplication
+
+// types of templates
+// 1. Function Templates - generic function that works with any data type - to reuse logic
+// 2. Class Templates - defines a generic class 
+//                    - to create data structures/utility classes that works with any type 
+
+
+#endif
