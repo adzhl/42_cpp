cde5ef54a561e2805ef6c62f0be33addcc9d75a8
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-05-09 10:17:39 +0800

    Add ex00

--- module_07/ex00/include/whatever.hpp
+++ module_07/ex00/include/whatever.hpp
@@ -6,12 +6,14 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 14:58:45 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/05/08 15:24:16 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/05/09 10:15:03 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
+
+# include <iostream>
 
 // what is a template?
 // - allows you to write generic & usable code.
@@ -23,5 +25,21 @@
 // 2. Class Templates - defines a generic class 
 //                    - to create data structures/utility classes that works with any type 
 
+template <typename T>
+void swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b) {
+    return ((a < b) ? a : b);
+}
+
+template <typename T>
+const T& max(const T& a, const T& b) {
+    return ((a > b) ? a : b);
+}
 
 #endif
---
+++ module_07/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 10:13:14 by abinti-a          #+#    #+#             */
+/*   Updated: 2025/05/09 10:14:52 by abinti-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
