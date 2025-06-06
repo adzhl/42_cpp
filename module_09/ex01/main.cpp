5ff0acae05650852ec11a876f2999fba6438e472
Adya Zahila Hasman Binti Aidi Hasman <abinti-a@u90z05s03.42kl.edu.my>
2025-06-06 09:59:31 +0800

    Modify main

--- module_09/ex01/main.cpp
+++ module_09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abinti-a <abinti-a@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 10:27:55 by abinti-a          #+#    #+#             */
-/*   Updated: 2025/06/06 08:41:20 by abinti-a         ###   ########.fr       */
+/*   Updated: 2025/06/06 09:58:46 by abinti-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int main(int argc, char **argv) {
     RPN rpn;
     try {
         int result = rpn.evaluate(argv[1]);
-        std::cout << result << std::endl;
+        std::cout << result << '\n';
     } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
+        std::cerr << RED << e.what() << '\n' << RESET;
+        return (1);
     }
 
     return (0);
